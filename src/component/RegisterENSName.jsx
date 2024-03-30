65941ed7480160ff01980aa7dbcfb06d499c7df5
Victoria <adedayovicky123@gmail.com>
2024-03-30 16:58:32 +0100

    FEAT: successfully sent image to pinata

--- src/component/RegisterENSName.jsx
+++ src/component/RegisterENSName.jsx
@@ -5,10 +5,51 @@ import { Link } from "react-router-dom";
 
 const RegisterENSName = () => {
   const [ENSName, setENSName] = useState("");
-  const [ENSImage, setImageName] = useState("");
+  const [cid, setCid] = useState("");
 
-  const handleRegisterName = useRegisterENSName(ENSName, ENSImage);
+  const handleRegister = useRegisterENSName(
+    ENSName,
+    `${import.meta.env.VITE_GATEWAY_URL}/ipfs/${cid}`
+  );
 
+  const changeHandler = async (e) => {
+    await handleSubmission(e.target.files[0]);
+  };
+
+  const handleSubmission = async (fileToUpload) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", fileToUpload);
+      const metadata = JSON.stringify({
+        name: "File name",
+      });
+      formData.append("pinataMetadata", metadata);
+
+      const options = JSON.stringify({
+        cidVersion: 0,
+      });
+      formData.append("pinataOptions", options);
+
+      const res = await fetch(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
+          },
+          body: formData,
+        }
+      );
+
+      const resData = await res.json();
+      setCid(resData.IpfsHash);
+      console.log(resData.IpfsHash);
+    } catch (e) {
+      console.log(e);
+      alert("Trouble uploading file");
+    }
+  };
+  // resData.IpfsHash QmXg8DRYJf4bbtzvK8BKySKVnRL5YBoEsJUU1ir9F6CWZg
   return (
     <Flex direction={"column"} align={"center"}>
       <Flex width={"400px"} direction={"column"} align={"center"}>
@@ -29,21 +70,12 @@ const RegisterENSName = () => {
             <Text as="div" size="2" mb="1" weight="bold" align={"left"}>
               ENS Image
             </Text>
-            <input
-              type="file"
-              name=""
-              id=""
-              value={ENSImage}
-              onChange={(e) => setImageName(e.target.value)}
-            />
+            <input type="file" onChange={changeHandler} />
           </label>
         </Flex>
 
         <Flex gap="3" mt="4" justify="end">
-          <Button variant="soft" color="gray">
-            Cancel
-          </Button>
-          <Button className="text-black" onClick={handleRegisterName}>
+          <Button className="text-black" onClick={handleRegister}>
             <Link to={`/login`} className="text-black">
               Register
             </Link>
